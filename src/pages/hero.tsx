d2d573df05ce1a944e67500bd74aeab5b33e3ecc
Irsyad Al Ghifary <121652271+designdevlord@users.noreply.github.com>
2022-12-31 19:01:07 +0700

    add gradient

--- src/pages/hero.tsx
+++ src/pages/hero.tsx
@@ -1,4 +1,3 @@
-import { size } from "lodash";
 import React, { useMemo } from "react";
 
 interface HeroProps {
@@ -8,11 +7,28 @@ interface HeroProps {
 }
 
 export default function Hero(props: HeroProps) {
+  const lastWord = useMemo(
+    () => props.headline.split(" ").pop(),
+    [props.headline]
+  );
+
+  const headline = useMemo(() => {
+    const lastIndexOfSpace = props.headline.lastIndexOf(" ");
+    if (lastIndexOfSpace === -1) {
+      return props.headline;
+    }
+    return props.headline.substring(0, lastIndexOfSpace);
+  }, [props.headline]);
   return (
     <section className="flex px-24 pt-24">
-      <section className="w-3/6 mr-36">
+      <section className="w-4/6 mr-36">
         <p className="text-sm text-[#C4C4C4]">{props.miniHeadline}</p>
-        <h1 className="text-white text-[56px] ">{props.headline}</h1>
+        <h1 className="text-white text-[56px] ">
+          {headline}{" "}
+          <span className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#DD31D1] via-[#8343DA] to-[#3D84E7]">
+            {lastWord}
+          </span>
+        </h1>
         <p className="text-white opacity-70">{props.headlineDescription}</p>
       </section>
       <section className="w-3/6">
