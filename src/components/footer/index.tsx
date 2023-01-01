0bf96947b970dbad1ddf748b649fc0fb166c0689
Irsyad Al Ghifary <121652271+designdevlord@users.noreply.github.com>
2023-01-01 21:24:37 +0700

    add footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,3 +1,4 @@
+import { FOOTER } from "@utils/footer";
 import React from "react";
 
 export default function Footer() {
@@ -5,12 +6,26 @@ export default function Footer() {
     <div className="px-24 my-24 flex">
       <div className="w-2/6">
         <h2 className="gradient-text text-2xl inline">GROOVE.</h2>
-        <p className="text-white opacity-70 font-light">
+        <p className="text-white opacity-70 font-ligh mt-6">
           Wanna build something great together?
         </p>
       </div>
-      <div className="w-4/6">
-        <h5 className="gradient-text inline">Email</h5>
+      <div className="w-4/6 flex">
+        {FOOTER.FOOTER_ITEMS.map((items, idx) => (
+          <div key={idx} className="w-1/4">
+            <h5 className="gradient-text text-start inline">{items.TITLE}</h5>
+            <ul className="text-white flex flex-col gap-4 mt-6">
+              {items.LINKS.map((link, idx) => (
+                <li
+                  key={idx}
+                  className="text-white opacity-70 font-light hover:opacity-100 transition duration-150 ease-out hover:ease-in"
+                >
+                  <a href={link.url}>{link.label}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
       </div>
     </div>
   );
---
+++ src/utils/footer.ts
@@ -0,0 +1,52 @@
+export const FOOTER = {
+  FOOTER_ITEMS: [
+    {
+      TITLE: "Email",
+      LINKS: [
+        {
+          label: "hello@groove.studio",
+          url: "mailto:hello@groove.studio",
+        },
+      ],
+    },
+    {
+      TITLE: "Home",
+      LINKS: [
+        {
+          label: "Product",
+          url: "/",
+        },
+        {
+          label: "Project",
+          url: "/",
+        },
+      ],
+    },
+    {
+      TITLE: "About",
+      LINKS: [
+        {
+          label: "Contact Us",
+          url: "/",
+        },
+        {
+          label: "License",
+          url: "/",
+        },
+        {
+          label: "Privacy Policy",
+          url: "/",
+        },
+      ],
+    },
+    {
+      TITLE: "Address",
+      LINKS: [
+        {
+          label: "Bandung, West Java",
+          url: "/",
+        },
+      ],
+    },
+  ],
+};
